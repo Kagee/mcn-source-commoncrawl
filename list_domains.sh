2f26157515b61fc7f9bba6b96bf58c2f60e32577
Anders Einar Hilden <hildenae@gmail.com>
2018-06-07 19:34:47 +0200

    Pretty up code, reuse old list as input if exists

--- list_domains.sh
+++ list_domains.sh
@@ -1,22 +1,24 @@
 #! /usr/bin/env bash
-# Info: http://data.norge.no/data/registerenheten-i-br%C3%B8nn%C3%B8ysund/enhetsregisteret
-# Download: http://hotell.difi.no/download/brreg/enhetsregisteret?download
+# Info: 
+# Download: 
 SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "${SOURCE_DIR}"
-source "config.sh" # MCN_TOOLS, DOMAINS
+cd "$SOURCE_DIR" || exit
+source "./config.sh"
 
-if [ ! -e "${DOMAINS}" ] || [ "x$1" = "x--update-list" ]; then
-    echo "INFO: ${DOMAINS} not found, extracting ..." 1>&2
+if [ ! -e "$DOMAINS" ] || [ "x$1" = "x--update-list" ]; then
+    echo >&2 "[INFO] $DOMAINS not found, extracting ..."
     # We do some hacks to remove the number of false positive domains
     # urldecode: 20finn.no -> 
     #     http://blogg.no/share?url=http://pitoresk.blogg.no/1297370777_10feb2011.html&title=Antikviteter%20fra%20finn.no
     # sed: 300367.no -> 
     #     http://www.tromso.kommune.no/skriftlige-spoersmaal.300367.no.html
-    find "${STORAGE_PATH}/" -type f -name '*.gz' -exec zcat '{}' \; | \
+    # TODO: rewrite to work on one and one fiel, and to skip a file of a precessed file exsists
+    find "$STORAGE_PATH/" -type f -name '*.gz' -exec zcat '{}' \; | \
         sed -e 's/\.no\.html/.html/g' | \
-        ${MCN_TOOLS}/urldecode | ${MCN_TOOLS}/urldecode | ${MCN_TOOLS}/urldecode | \
-        ${MCN_TOOLS}/default_extract > "${DOMAINS}"
+        "$MCN_TOOLS/urldecode" 3 | \
+        "$MCN_TOOLS/default_extract" | cat - "$DOMAINS" 2>/dev/null > "_tmp.list"
+    mv "_tmp.list" "$DOMAINS"
 else
-    echo "INFO: ${DOMAINS} found. Use '$0 --update-list' to force new extraction." 1>&2
+    echo >&2 "[INFO] $DOMAINS found. Use '$0 --update-list' to force new extraction."
 fi
 #cat "${DOMAINS}"
