870e0c78084aa1b943df77c7306f861e3faf6c15
Anders Einar Hilden <hildenae@gmail.com>
2018-06-16 22:25:28 +0200

    Better output messages

--- verify-indexes.sh
+++ verify-indexes.sh
@@ -5,4 +5,7 @@ cd "${SOURCE_DIR}"
 source "config.sh"
 
 find "${STORAGE_PATH}/" -type f -name '*.gz' | \
- parallel gzip --test {}
+  parallel gzip --test {} 2>&1 | O="$(cat -)"
+
+echo -n "Number of files with errors: "; echo "$O" | grep -c 'gzip:'
+echo "$O"
