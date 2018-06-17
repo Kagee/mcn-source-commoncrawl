4f4f1bc821ba27b0860b183e08b7974951cb3959
Anders Einar Hilden <hildenae@gmail.com>
2018-06-17 23:04:45 +0200

    Paralell extracting of domains

--- 20-list_domains.sh
+++ 20-list_domains.sh
@@ -8,37 +8,58 @@ source "./config.sh"
 set -euo pipefail
 #set -x
 
+LOG_INFO=1
+LOG_VERB=1
 
-COUNT="$(find . -name "*$DOMAINS" 2>/dev/null | wc -l)";
-if [ "$COUNT" -eq "0" ] || [ "x${1:-}" = "x--update-list" ]; then
-    echo >&2 "[INFO] $DOMAINS not found, extracting ..."
+function def_ex {
+  GZ_FILE="${1:-}"
+  MCN_TOOLS="${3:-}"
+  GZ_HASH="$(md5sum "$GZ_FILE" | cut -d' ' -f 1)"
+  CACHE_FOLDER="${2:-}/${GZ_HASH:0:1}"  
+  CACHE_FILE="$CACHE_FOLDER/$GZ_HASH";
+  JOD_ID="${4:-}"
+  if [ ! -f "$CACHE_FILE.xz" ]; then
+    INFO "Extracting domains from $GZ_FILE to $CACHE_FILE.xz"
+    mkdir -p "$CACHE_FOLDER"
     # We do some hacks to remove the number of false positive domains
     # urldecode: 20finn.no -> 
     #     http://blogg.no/share?url=http://pitoresk.blogg.no/1297370777_10feb2011.html&title=Antikviteter%20fra%20finn.no
     # sed: 300367.no -> 
     #     http://www.tromso.kommune.no/skriftlige-spoersmaal.300367.no.html
-    TIMESTAMP="$(date +%F%T | tr ':' '-')"
-    mkdir -p "$STORAGE_PATH/no-cache"
-
-    find "$STORAGE_PATH/" -type f -name '*.gz' > _tmp.gzs
-    while read -r FILE; do
-          MD="$STORAGE_PATH/no-cache/$(md5sum "$FILE" | cut -d' ' -f 1).xz";
-          if [ ! -f "$MD" ]; then
-            echo >&2 "[INFO] Processing $FILE into $MD";
-            zcat "$FILE" | sed -e 's/\.no\.html/.html/g' | \
-            "$MCN_TOOLS/urldecode" 3 | \
-            "$MCN_TOOLS/default_extract" | \
-            xz --compress --stdout > "$MD.tmp" && mv "$MD.tmp" "$MD"
-          else
-            echo >&2 "[INFO] Found $MD, not processing $FILE"
-          fi
-    done < _tmp.gzs
-  exit
-      #  && \
-      find "$STORAGE_PATH/no-cache" -name '*.xz' -exec xzcat '{}' \;| \
+    zcat "$GZ_FILE" | sed -e 's/\.no\.html/.html/g' | \
+      "$MCN_TOOLS/urldecode" 3 | \
       "$MCN_TOOLS/default_extract" | \
-      cat - ./*"$DOMAINS" || true  | sort | uniq > "_tmp.list" && \
-      mv "_tmp.list" "$TIMESTAMP-$DOMAINS"
+      sort | uniq | \
+      xz --compress --stdout > "$CACHE_FILE.tmp" && \
+      mv "$CACHE_FILE.tmp" "$CACHE_FILE.xz"
+  else
+    VERB "Found $CACHE_FILE.xz, not extracting domains from $GZ_FILE"
+  fi
+}
+
+export -f def_ex
+export LOG_INFO
+export LOG_VERB
+
+COUNT="$(find . -name "*$DOMAINS" 2>/dev/null | wc -l)";
+if [ "$COUNT" -eq "0" ] || [ "x${1:-}" = "x--update-list" ]; then
+  INFO "$DOMAINS not found, extracting ..."
+
+  TIMESTAMP="$(date +%F-%T | tr ':' '-')"
+  EXTRACT_CACHE="$STORAGE_PATH/ext-cache"
+  mkdir -p "$EXTRACT_CACHE"
+  GZ_FILES="$(mktemp "$PWD/$TIMESTAMP-XXXXXXXXXX")"
+  find "$STORAGE_PATH/" -type f -name '*.gz' > "$GZ_FILES"
+  cat "$GZ_FILES" | parallel def_ex '{}' "$EXTRACT_CACHE" "$MCN_TOOLS" '{#}'
+  echo "$GZ_FILES"
+  rm "$GZ_FILES"
+  #find "$STORAGE_PATH/no-cache" -name '*.xz' | \
+  #sort | uniq | parallel -P "$(echo "$(nproc --all) * 2" | bc)" def_ex '{#}' '{}'
+  #exit 
+  #  cat ./*"$DOMAINS" > "_tmp.list"
+  #  find "$STORAGE_PATH/no-cache" -name '*.no' > "_tmp.list"
+  #  cat "_tmp.list" | sort | uniq > "_tmp.list2" && mv "_tmp.list2" "$TIMESTAMP-$DOMAINS"
+  #  rm "_tmp.list"
 else
-    echo >&2 "[INFO] $DOMAINS found. Use '$0 --update-list' to force new extraction."
+    INFO "$DOMAINS found. Use '$0 --update-list' create a new extract."
 fi
