58bdfaa994b2a9a29f19794e6c85fc457dad3e57
Anders Einar Hilden <hildenae@gmail.com>
2019-05-07 13:29:29 +0200

    Read commoncrawler JSON instead of scarping web page

--- 00-get-indexes.sh
+++ 00-get-indexes.sh
@@ -7,28 +7,34 @@ CLIENT="cdx-index-client/cdx-index-client.py"
 WD="$PWD"
 
 #echo -e "CC-MAIN-2013-20\nCC-MAIN-2013-48" | \
-curl -s http://index.commoncrawl.org/ | grep -o -P 'CC-MAIN-....-..' | sort | uniq | \
-while read API_ENDPOINT;
+#curl -s http://index.commoncrawl.org/ | grep -o -P 'CC-MAIN-....-[0-9]*' | sort | uniq | \
+curl -s http://index.commoncrawl.org/collinfo.json | jq -c '.[]' | sort | uniq | \
+while read JSON;
 do
-  FOLDER="${STORAGE_PATH}/$API_ENDPOINT"
+  ID="$(echo "$JSON" | jq -r '.["id"]')"
+  CDX="$(echo "$JSON" | jq -r '.["cdx-api"]')"
+  FOLDER="${STORAGE_PATH}/${ID}"
   METADATA="$FOLDER/metadata"
   mkdir -p "$FOLDER";
-  PAGES="$(cat ${FOLDER}/metadata | jq -r .pages)" 2>/dev/null
+  PAGES="$(cat ${FOLDER}/metadata | jq -r .pages 2>/dev/null)" 2>/dev/null
   if [ ! -f "${METADATA}" ] || [ "x$PAGES" = "x" ]; then
-    curl -s "http://index.commoncrawl.org/${API_ENDPOINT}-index?url=*.no&output=json&showNumPages=true" > "$METADATA"
+    1>&2 echo "[INFO] Metadata was missing, downloading"
+    curl -s "${CDX}?url=*.no&output=json&showNumPages=true" > "$METADATA"
     PAGES="$(cat ${FOLDER}/metadata | jq -r .pages)"
   fi
   FILES="$(ls $FOLDER | grep -v metadata | wc -l)"
-  #echo "Pages: $PAGES, Files: $FILES"
+  1>&2 echo "[INFO] ID: $ID, PAGES: $PAGES, Files: $FILES"
 
   if [ $PAGES -eq $FILES ]; then
-    echo "Found $(find $FOLDER -type f ! -name metadata | wc -l) of $(cat ${FOLDER}/metadata| jq -r .pages) expected files for $API_ENDPOINT, assuming complete"
+    echo "Found $(find $FOLDER -type f ! -name metadata | wc -l) of $(cat ${FOLDER}/metadata| jq -r .pages) expected files for ${ID}, assuming complete"
   else
-    echo -n "$API_ENDPOINT ";
-    cat "${STORAGE_PATH}/${API_ENDPOINT}/metadata";
+    1>&2 echo "Found $(find $FOLDER -type f ! -name metadata | wc -l) of $(cat ${FOLDER}/metadata| jq -r .pages) expected files for ${ID}, downloading"
+    1>&2 echo -n "Downloading pages for $ID ";
+    1>&2 echo $JSON
+    1>&2 cat "${METADATA}";
     cd "${FOLDER}";
     # Defaults to Commoncrawer, so we don't supply a server URL
-    ${WD}/${CLIENT} --processes ${THREADS} --coll "${API_ENDPOINT}" '*.no' --fl url -z;
+    ${WD}/${CLIENT} --processes ${THREADS} --coll "${ID}" '*.no' --fl url -z;
     cd "$WD";
   fi
   #sleep 5;
